b94b34b79abd83384d0f6c58e1085415ae72d607
Kris Henderson <krishenderson@gmail.com>
2020-02-09 16:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Code your solution here!
 
+const printString = (string) => {
+  if(string.length < 2) {
+    console.log(string);
+  }
+  console.log(printString(string.split("").splice(string.length - 1, 1).join("")))
+}
